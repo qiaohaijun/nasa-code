9ccc9643414b0873f65bd46d78e1e756bc62d13d
qiaohaijun <qiaohaijun@sogou-inc.com>
2019-03-06 11:22:42 +0800

    0009-palindrome-number

---
+++ 0009-palindrome-number.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        # guard phase
+        if x<0:
+            return False
+
+        s1 = str(x)
+
+        l = 0
+        r = len(s1)-1
+
+        while l<r:
+            if s1[l]!=s1[r]:
+                return False
+            l=l+1
+            r=r-1
+
+        return True
