27f3435b6970574cdf1ad1c4d00158736761cdde
qiaohaijun <qiaohaijun@sogou-inc.com>
2019-03-06 11:24:09 +0800

    0009-palindrome-number

--- 0009-palindrome-number.py
+++ 0009-palindrome-number.py
@@ -5,18 +5,18 @@ class Solution(object):
         :rtype: bool
         """
         # guard phase
-        if x<0:
+        if x < 0:
             return False
 
         s1 = str(x)
 
         l = 0
-        r = len(s1)-1
+        r = len(s1) - 1
 
-        while l<r:
-            if s1[l]!=s1[r]:
+        while l < r:
+            if s1[l] != s1[r]:
                 return False
-            l=l+1
-            r=r-1
+            l = l + 1
+            r = r - 1
 
         return True
